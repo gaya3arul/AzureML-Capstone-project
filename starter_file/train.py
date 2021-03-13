22ee52e94253e9252a6ce021c496b0fff14e4f10
Gayathri Venkatesh <gaya3.venkatesh@gmail.com>
2021-03-13 18:11:00 +0530

    Add files via upload

--- starter_file/train.py
+++ starter_file/train.py
@@ -5,43 +5,33 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from argparse import ArgumentParser
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-def clean_data(data):
-    # Dict for cleaning data
+# Create TabularDataset using TabularDatasetFactory
+ds = pd.read_csv('./heart_failure_clinical_records_dataset.csv')
 
+# Preview of the first five rows
+ds.head()
 
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    Operator = pd.get_dummies(x_df.Operator, prefix="job")
-    x_df.drop("Operator", inplace=True, axis=1)
-    x_df.drop("Date", inplace=True, axis=1)
-    
+# Explore data
+ds.describe()
 
-    y_df = x_df.pop("Failure").apply(lambda s: 1 if s == "Yes" else 0)
-    return x_df,y_df
+# Data columns
+ds.columns = ['age', 'anaemia', 'creatinine_phosphokinase', 'diabetes', 'ejection_fraction', 'high_blood_pressure', 'platelets', 'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time', 'DEATH_EVENT']
+x = ds[['age', 'anaemia', 'creatinine_phosphokinase', 'diabetes', 'ejection_fraction', 'high_blood_pressure', 'platelets', 'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time']]
+y = ds[['DEATH_EVENT']]
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-datastore_path="https://raw.githubusercontent.com/gaya3arul/nd00333-capstone/master/train.csv"
+# Split data into train and test sets.
+# Documentation: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-ds = TabularDatasetFactory.from_delimited_files(path=datastore_path)
-
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-#dividing X,y into train and test data
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=403)
-
-### YOUR CODE HERE ###a
+data = {"train": {"X": x_train, "y": y_train},
+        "test": {"X": x_test, "y": y_test}}
 
 run = Run.get_context()
-
 
 def main():
     # Add arguments to script
@@ -59,7 +49,9 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-    os.makedirs('outputs', exist_ok=True)    
+
+    os.makedirs('outputs', exist_ok=True)
+
     joblib.dump(value=model, filename='outputs/model.pkl')
 
 if __name__ == '__main__':
